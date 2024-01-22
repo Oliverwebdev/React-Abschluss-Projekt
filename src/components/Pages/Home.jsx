7d862b1a252f329ed43a92a983e65010de560876
dci-student <oliver.sporl@dci-student.org>
2024-01-22 14:44:31 +0100

    array of best games implement

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../PageLayout/Header/Header";
+import Indexmain from "../buildcomponents/Indexmain";
 
 function Home() {
   return (
     <div>
       <Header />
+      <Indexmain />
     </div>
   );
 }
---
+++ src/components/buildcomponents/Indexmain.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import BestGamesEver from '../../apis/BestGamesFetch';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  background-color: #333;
+  color: white;
+  padding: 20px;
+  text-align: center;
+  width: 100%;
+`;
+
+const CarouselContainer = styled.div`
+  display: flex;
+  
+  justify-content: space-between;
+  overflow-x: auto;
+  margin-top: 20px; /* Abstand nach oben hinzufügen */
+`;
+
+function Indexmain() {
+  return (
+    <Div>
+      <h2>Die besten Spiele aller Zeiten</h2>
+      <CarouselContainer>
+        {/* Hier werden die Spiele angezeigt */}
+        <BestGamesEver />
+        {/* Füge weitere Spiele hinzu, wenn nötig */}
+      </CarouselContainer>
+    </Div>
+  );
+}
+
+export default Indexmain;
+
