5889f25903dc4769284cafd9b19ca31376bb922e
dci-student <hugon1992@gmail.com>
2024-01-22 13:49:46 +0100

    PCGamesFetch Component added to /apis

---
+++ src/apis/PcGamesFetch.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+
+const PcGamesFetch = () => {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiKey = "e5af9c0ecbb74eb68b32eb1dc1142b2b"; // Cseréld le a saját API kulcsodra
+        const platforms = "4"; // PC platform kódja a RAWG API-ban: 4
+        const pageSize = 10; // Opcionális: lekérdezési oldalonkénti játékok száma
+
+        const response = await fetch(
+          `https://api.rawg.io/api/games?key=${apiKey}&platforms=${platforms}&page_size=${pageSize}`
+        );
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setGames(data.results);
+      } catch (error) {
+        console.error("Error:", error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>PC Gaames</h1>
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PcGamesFetch;
