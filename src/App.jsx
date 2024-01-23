9bf14813fd5f0f6e91096947829a200a098f8569
bita9170 <bita.s9170@gmail.com>
2024-01-24 00:56:38 +0100

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Home from "./pages/Home";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import { Routes, Route } from "react-router-dom";
 import PlayStation from "./pages/PlayStation";
 import Xbox from "./pages/Xbox";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/ps" element={<PlayStation />} />
         <Route path="/xbox" element={<Xbox />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
