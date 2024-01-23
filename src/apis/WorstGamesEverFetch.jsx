37f8e64bbbfea78f16729e3148168fb2ac074ab1
dci-student <oliver.sporl@dci-student.org>
2024-01-23 11:12:16 +0100

    apis edited layout

--- src/apis/WorstGamesEverFetch.jsx
+++ src/apis/WorstGamesEverFetch.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import Slider from 'react-slick';
+import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const WorstGamesEver = () => {
   const apiKey = "e5af9c0ecbb74eb68b32eb1dc1142b2b";
@@ -12,7 +12,9 @@ const WorstGamesEver = () => {
   useEffect(() => {
     const fetchWorstGames = async () => {
       try {
-        const response = await fetch(`${apiUrl}?key=${apiKey}&ordering=metacritic`);
+        const response = await fetch(
+          `${apiUrl}?key=${apiKey}&ordering=metacritic`
+        );
         const data = await response.json();
         console.log("API-Antwort:", data);
         if (!data || !data.results || data.results.length === 0) {
@@ -41,17 +43,17 @@ const WorstGamesEver = () => {
     <div>
       {worstGames.length > 0 ? (
         <div>
-          <h2>Die schlechtesten Spiele aller Zeiten</h2>
+          <h2 style={{ margin: "2rem", textAlign: 'center' }}>Die schlechtesten Spiele aller Zeiten</h2>
           <Slider {...settings}>
             {worstGames.map((game) => (
-              <div key={game.id}>
+              <div key={game.id} style={{ textAlign: 'center' }}>
                 <h3>{game.name}</h3>
                 <p>Bewertung: {game.metacritic}%</p>
                 {/* Weitere Informationen hier einfügen */}
                 <img
                   src={game.background_image}
                   alt={game.name}
-                  style={{ maxWidth: '150px', maxHeight: '150px' }}
+                  style={{ width: "180px", height: "100px", margin: '0 auto' }}
                 />
               </div>
             ))}
