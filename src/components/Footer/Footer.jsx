880253431b092d5343134a280262aff0f725910c
bita9170 <bita.s9170@gmail.com>
2024-01-24 01:15:34 +0100

    added text in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Footer() {
   return (
-<div>
+<div> 
 <footer className="footer p-10 bg-base-200 text-base-content">
   <nav>
     <header className="footer-title">Services</header> 
@@ -12,7 +12,7 @@ function Footer() {
     <a className="link link-hover">Marketing</a>
     <a className="link link-hover">Advertisement</a>
   </nav> 
-  <nav>
+  <nav> <h1>****Nur als Beispiel****</h1>
     <header className="footer-title">Company</header> 
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
