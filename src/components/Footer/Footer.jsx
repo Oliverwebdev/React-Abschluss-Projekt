fc96b4b1c6daaf6151b83bfb86275157819aa92f
bita9170 <bita.s9170@gmail.com>
2024-01-25 22:41:12 +0100

    added footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,35 +1,13 @@
 import React from "react";
+import Nav from "./Nav";
+import Team from "./Team";
 
 function Footer() {
   return (
-<div className="footer-container flex justify-center flex-wrap mt-20 ml-20">
-  <div className="footer-column">
-    <h4 className="text-lg font-bold mb-2">Github Profiles</h4>
-    <ul className="list-none p-0">
-      <li><a href="https://github.com/user1" className="text-blue-500 hover:underline">User 1</a></li>
-      <li><a href="https://github.com/user2" className="text-blue-500 hover:underline">User 2</a></li>
-      <li><a href="https://github.com/user3" className="text-blue-500 hover:underline">User 3</a></li>
-    </ul>
-  </div>
-
-  <div className="footer-column">
-    <h4 className="text-lg font-bold mb-2">Styling Themes</h4>
-    <ul className="list-none p-0">
-      <li><a href="/theme1" className="text-blue-500 hover:underline">Theme 1</a></li>
-      <li><a href="/theme2" className="text-blue-500 hover:underline">Theme 2</a></li>
-      <li><a href="/theme3" className="text-blue-500 hover:underline">Theme 3</a></li>
-      <li><a href="/theme4" className="text-blue-500 hover:underline">Theme 4</a></li>
-    </ul>
-  </div>
-
-  <div className="footer-column">
-    <h4 className="text-lg font-bold mb-2">RAWG API</h4>
-    <a href="https://rawg.io" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">RAWG</a>
-  </div>
-</div>
-
-
-
+    <footer>
+      <Team />
+      <Nav />
+    </footer>
   );
 }
 
--- src/components/Footer/Nav.jsx
+++ src/components/Footer/Nav.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import XBox from "/src/images/XBox.png";
+import PS from "./../../images/PlayStation.png";
+import Pc from "./../../images/Pc.jpg";
+import Epic from "./../../images/Epic.jpg";
+import Riot from "./../../images/Riot.png";
+import Steam from "./../../images/Steam.jpg";
 
 function Nav() {
   return (
-    <div>Nav</div>
-  )
+    <nav className="game-container">
+      <h2>Game Store</h2>
+
+      <ul>
+        <li>
+          <img src={XBox} alt="" />
+        </li>
+
+        <li>
+          <img src={PS} alt="" />
+        </li>
+
+        <li>
+          <img className="w-12" src={Pc} alt="" />
+        </li>
+
+        <li>
+          <img src={Epic} alt="Epic" />
+        </li>
+
+        <li>
+          <img src={Riot} alt="Riot" />
+        </li>
+
+        <li>
+          <img src={Steam} alt="Steam" />
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
