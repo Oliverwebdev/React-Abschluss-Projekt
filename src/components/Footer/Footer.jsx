fda6fc8d2494fd3c9e547402c51bacfdfdb1d4ae
bita9170 <bita.s9170@gmail.com>
2024-01-25 23:29:13 +0100

    added newsletter component & daisy styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "./Nav";
 import Team from "./Team";
+import Newsletter from "./Newsletter";
 
 function Footer() {
   return (
     <footer>
       <Team />
       <Nav />
+      <Newsletter />
     </footer>
   );
 }
