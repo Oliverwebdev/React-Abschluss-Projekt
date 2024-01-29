11b080f2e064f0db1eea48e61da8f0463a6f4ec0
dci-student <oliver.sporl@dci-student.org>
2024-01-29 11:33:10 +0100

    deploy index fixing

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig (({command} )=> {
 const config = {
   plugins: [react()],
-  base: command === "serve" ? "/" : "/React-Abschluss-Projekt/",
+  base: command === "serve" ? "/React-Abschluss-Projekt/" : "/React-Abschluss-Projekt/",
   };
   return config;  
 });
