ee4858408cb0644dd5eaf694098c40dc95602d6b
dci-student <hugon1992@gmail.com>
2024-01-29 12:03:09 +0100

    router config edit

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base:
-      command === "serve"
-        ? "/React-Abschluss-Projekt/"
-        : "/React-Abschluss-Projekt/",
+    base: command === "serve" ? "/React-Abschluss-Projekt/" : "/",
   };
   return config;
 });
