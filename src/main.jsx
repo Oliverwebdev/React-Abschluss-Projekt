20d965d69df6f757d63099f26ef8d76239be1c4e
dci-student <oliver.sporl@dci-student.org>
2024-01-29 13:21:15 +0100

    deploy fixed

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename={import.meta.env.DEV ? "/React-Abschluss-Projekt/" : "/"}>
+    <Router basename={import.meta.env.DEV ? "/" : "/React-Abschluss-Projekt/"}>
       <App />
     </Router>
   </React.StrictMode>,
